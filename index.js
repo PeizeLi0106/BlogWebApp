11ecf8936394177d7ea9fba36dc5b75fd9a98b6c
Peize Li <peizeli@Peizes-MacBook-Air.local>
2024-08-02 11:55:23 +0800

    finished implementing basic functionality

--- index.js
+++ index.js
@@ -17,19 +17,44 @@ app.use(bodyParser.urlencoded({ extended: true })); // deals with readable body 
 app.get("/", (req, res) => {
     res.render("index.ejs", {posts: posts}); // render the home page and send an array of posts to the backend
 });
-
+app.get("/search", (req, res) => {
+    let searchingTitle = req.query.q;
+    console.log(searchingTitle);
+    let notFound = true;
+    for (var i = 0; i < posts.length; i++) {
+        if (posts[i].title == searchingTitle) {
+            //console.log("found a matching title");
+            notFound = false;
+            res.render("post.ejs", {post: posts[i], postid: i});
+        }
+    }
+    if (notFound){
+        console.log("result not found");
+        res.redirect("/");
+    }
+})
 app.post("/", (req, res) => {
     posts.push(req.body);
     res.redirect("/");
 });
-app.get("/:postid", (req, res) => {
-    res.render("post.ejs", {post: posts[req.params.postid]});
-})
-app.patch("/editPost", (req, res) => {
-    //console.log(req.body);  
+
+app.post("/:postid", (req, res) => {
+    let postid = req.params.postid;
+    posts[postid] = req.body;
+    res.redirect("/");
 });
-app.delete("/deletePost", (req, res) => {
-    //console.log(req.body);
+
+app.get("/:postid", (req, res) => {
+    let postid = req.params.postid;
+    res.render("post.ejs", {post: posts[postid], postid: postid});
+})
+app.post("/editPost/:postid", (req, res) => {
+      let postid = req.params.postid;
+      res.render("edit.ejs", {post: posts[postid], postid: postid});
+});
+app.post("/deletePost/:postid", (req, res) => {
+    posts.splice(req.params.postid, 1); // deletes the post at postid from the posts array
+    res.redirect("/");
 });
 
 app.listen(port, () => {
